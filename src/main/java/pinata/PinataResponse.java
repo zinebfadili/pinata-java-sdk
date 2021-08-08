4adc3f381bef4d50e2bc61016e0b93fa962fe07b
zinebfadili <zineb.fadili@insa-lyon.fr>
2021-08-08 23:29:58 +0200

    rename msg to body

--- src/main/java/pinata/PinataResponse.java
+++ src/main/java/pinata/PinataResponse.java
@@ -6,7 +6,7 @@ package pinata;
 public class PinataResponse {
 
   private int status;
-  private String message;
+  private String body;
 
   public PinataResponse() {
 
@@ -14,22 +14,22 @@ public class PinataResponse {
 
   public PinataResponse(int status, String message) {
     this.status = status;
-    this.message = message;
+    this.body = message;
   }
 
   public int getStatus() {
     return status;
   }
 
-  public String getMessage() {
-    return message;
+  public String getBody() {
+    return body;
   }
 
   public void setStatus(int status) {
     this.status = status;
   }
 
-  public void setMessage(String message) {
-    this.message = message;
+  public void setBody(String body) {
+    this.body = body;
   }
 }
